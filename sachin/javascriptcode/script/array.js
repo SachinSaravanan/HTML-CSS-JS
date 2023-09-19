2f0ec73543f32dfd6e31992e1248e27595680328
unknown <ssachinsachin113@gmail.com>
2023-09-19 20:31:51 +0530

    sachin.html

---
+++ sachin/javascriptcode/script/array.js
@@ -0,0 +1,49 @@
+
+
+// function checkNumber(number) {
+//   if (5 > 0) {
+//     console.log("The number is positive.");
+//   } else if (-2< 0) {
+//     console.log("The number is negative.");
+//   } else {
+//     console.log( "The number is zero."+number);
+//   }
+// }
+
+// // Example usage:
+// checkNumber(5);   // Output: The number is positive.
+// // checkNumber(-2);  // Output: The number is negative.
+// // checkNumber(0);   // Output: The number is zero.
+// 1. Create an empty array called "cars".
+// let cars = [];
+
+// // 2. Add three car objects to the "cars" array.
+// for (let i = 0; i < 3; i++) {
+//   let car = {
+//     make: "Toyota",
+//     model: "Camry",
+//     year: 2018
+//   };
+//   cars.push(car);
+// }
+
+// // 3. Remove the first car object from the "cars" array.
+// if (cars.length > 0) {
+//   cars.shift();
+// }
+
+// // 4. Add a new car object to the "cars" array.
+// let newCar = {
+//   make: "Honda",
+//   model: "Civic",
+//   year: 2020
+// };
+// cars.push(newCar);
+
+// // 5. Update the "model" property of the second car object in the array to "Accord".
+// if (cars.length >= 2) {
+//   cars[1].model = "Accord";
+// }
+
+// // Print the final "cars" array after performing the above operations.
+// console.log(cars);

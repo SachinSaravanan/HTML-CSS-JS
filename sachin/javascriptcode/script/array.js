4654a332d2dcf6ef2b8738249c2b7a84d531aa82
unknown <ssachinsachin113@gmail.com>
2023-09-23 16:33:13 +0530

    sachin.html

--- sachin/javascriptcode/script/array.js
+++ sachin/javascriptcode/script/array.js
@@ -47,3 +47,41 @@
 
 // // Print the final "cars" array after performing the above operations.
 // console.log(cars);
+// 1. Create an empty array called "cars".
+let cars = [];
+
+// 2. Add three car objects to the "cars" array.
+// Each car object should have the specified properties.
+cars.push({
+  make: "Toyota",
+  model: "Camry",
+  year: 2018
+});
+cars.push({
+  make: "Toyota",
+  model: "Camry",
+  year: 2018
+});
+cars.push({
+  make: "Toyota",
+  model: "Camry",
+  year: 2018
+});
+
+// 3. Remove the first car object from the "cars" array.
+cars.shift();
+
+// 4. Add a new car object to the "cars" array with the specified properties.
+cars.push({
+  make: "Honda",
+  model: "Civic",
+  year: 2020
+});
+
+// 5. Update the "model" property of the second car object in the array to "Accord".
+if (cars.length >= 2) {
+  cars[1].model = "Accord";
+}
+
+// 6. Print the final "cars" array.
+console.log(cars);
